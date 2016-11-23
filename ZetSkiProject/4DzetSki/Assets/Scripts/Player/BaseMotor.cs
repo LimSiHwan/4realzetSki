2a8bf48421f0e00ba04aa87176237df8c8989846
LimSihwan <tlghks1009@naver.com>
2016-11-23 17:51:57 +0900

    제트스키 - 움직임(진행중)

---
+++ ZetSkiProject/4DzetSki/Assets/Scripts/Player/BaseMotor.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class BaseMotor : MonoBehaviour
+{
+	protected CharacterController controller;
+	protected Transform thisTransform;
+
+	 //속도, 점프 높이
+	private float BaseSpeed = 5.0f;
+	private float BaseJump = 5.0f;
+	
+	public float Speed {get { return BaseSpeed;} set { BaseSpeed = value; }}
+	public float Jump {get {return BaseJump; } }
+
+	// 움직이는 방향
+	protected Vector3 InputDirection { get;set; } 
+	
+	// 움직임
+	protected abstract void MoveDirection();
+
+	protected virtual void Start()
+	{
+		controller = gameObject.AddComponent<CharacterController>();
+		thisTransform = transform;
+	}
+
+	void Update()
+	{
+		MoveDirection();
+	}
+	
+	protected virtual void Move()
+	{
+		controller.Move(InputDirection * Time.deltaTime);
+	}
+}
---
+++ ZetSkiProject/4DzetSki/Assets/Scripts/Player/PlayerMotor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class PlayerMotor:BaseMotor
+{
+	protected override void MoveDirection()
+	{
+		//방향을 넣어줌
+		InputDirection = InputDir();
+
+		//움직임.
+		Move(); 
+	}
+
+	private Vector3 InputDir()
+	{
+		Vector3 dir = Vector3.zero;
+
+		dir.x = Input.GetAxis("Horizontal");
+		dir.z = Input.GetAxis("Vertical");
+
+		if(dir.magnitude > 1)
+			dir.Normalize();
+
+		return Vector3.zero;
+	}
+}
